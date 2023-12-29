e399c249326536764ec2720718e61b2fce9fe4b8
zjordanRRC <zjordan3@rrc.ca>
2023-12-28 21:04:36 -0700

    this commit is styling the landing page of the site

--- src/welcome.js
+++ src/welcome.js
@@ -1,10 +1,12 @@
+import './styles/welcome.css';
+
 function Welcome() {
     return(
-        <>
-        <h1>Welcome to Task K9</h1>
-        <h2>Join a growing and loving community of working dog owners, handlers, and decoys!</h2>
-        <p>Task K9's mission is bring the working dog community closer together with one easy stop.</p>
-        </>
+        <div className='welcome'>
+            <h1>Welcome to Task K9</h1>
+            <h2>Join a growing and loving community of working dog owners, handlers, and decoys!</h2>
+            <p>Task K9's mission is bring the working dog community closer together with one easy stop.</p>
+        </div> 
     );
 }
 
